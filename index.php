4935c4fd2cf5cfb23a0c9f24c05d4e016a84b1e5
Celsoib <90781135+Celsoib@users.noreply.github.com>
2022-01-20 18:22:39 -0300

    Sección 41: Estructura HTML de la aplicación web
    Cabecera, nav, contenido principal, pie de página

--- index.php
+++ index.php
@@ -1,37 +1,123 @@
-<?php
-  date_default_timezone_set('America/Asuncion');
-  // Conectar a la base de datos
-  $conexion = mysqli_connect("localhost","root","","phpmysql");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="./assets/css/estilo.css">
+</head>
+<body>
+  <!-- Cabecera  -->
+  <header id="cabecera">
+    <!-- logo  -->
+    <div id="logo">
+      <a href="index.php">
+        Blog de videojuegos
+      </a>
+    </div>
+    <!-- Menu  -->
+    <nav id="menu">
+      <ul>
+        <li>
+          <a href="index.php">Inicio</a>
+        </li>
+        <li>
+          <a href="index.php">Categoría 1</a>
+        </li>
+        <li>
+          <a href="index.php">Categoría 2</a>
+        </li>
+        <li>
+          <a href="index.php">Categoría 3</a>
+        </li>
+        <li>
+          <a href="index.php">Categoría 4</a>
+        </li>
+        <li>
+          <a href="index.php">Sobre mí</a>
+        </li>
+        <li>
+          <a href="index.php">Contacto</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 
-  // Consulta para configurar la codificación de caracter
-  mysqli_query($conexion, "SET NAMES 'utf8'");
+  <div id = "contenedor">
+    <!-- Barra lateral  -->
+    <aside id = "sidebar">
+      <div id = "login" class = "bloque">
+        <h3>Identifícate</h3>
+        <form action="login.php" method="POST">
+          <p>
+            <label for="email">Email</label>
+            <input type="email" name="email">
+          </p>
+          <p>
+            <label for="password">Contraseña</label>
+            <input type="password" name="password">
+          </p>
+          <input type="submit" value="Entrar">
+        </form>
+      </div>
+      <div id = "register" class = "bloque">
+        <h3>Regístrate</h3>
+        <form action="registro.php" method="POST">
+          <p>
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre">
+          </p>
+          <p>
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos">
+          </p>
+          <p>
+            <label for="email">Email</label>
+            <input type="email" name="email">
+          </p>
+          <p>
+            <label for="password">Contraseña</label>
+            <input type="password" name="password">
+          </p>
+          <input type="submit" value="Registrar">
+        </form>
+      </div>
+    </aside>
 
-  // Comprobar si la conexión es correcta
-  if(mysqli_connect_errno()){
-    echo "La conexión a la bd mysql ha fallado: ".mysqli_connect_error();
-  } else {
-    echo "Conexión realizada correctamente";
-  }
+    <!-- Caja principal  -->
+    <div id="principal">
+      <h1>Últimas entradas</h1>
+      <article class="entrada">
+        <h2>Título de mi entrada</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga recusandae, incidunt tempora omnis culpa qui dignissimos ducimus numquam minima, enim beatae. Ipsa, distinctio modi sequi magnam eaque amet ad repudiandae.
+        </p>
+      </article>
+      <article class="entrada">
+        <h2>Título de mi entrada</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga recusandae, incidunt tempora omnis culpa qui dignissimos ducimus numquam minima, enim beatae. Ipsa, distinctio modi sequi magnam eaque amet ad repudiandae.
+        </p>
+      </article>
+      <article class="entrada">
+        <h2>Título de mi entrada</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga recusandae, incidunt tempora omnis culpa qui dignissimos ducimus numquam minima, enim beatae. Ipsa, distinctio modi sequi magnam eaque amet ad repudiandae.
+        </p>
+      </article>
+      <article class="entrada">
+        <h2>Título de mi entrada</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga recusandae, incidunt tempora omnis culpa qui dignissimos ducimus numquam minima, enim beatae. Ipsa, distinctio modi sequi magnam eaque amet ad repudiandae.
+        </p>
+      </article>
+    </div>
+  </div>
+  <!-- Pie de página  -->
+  <footer id="footer">
+    <p>Desarrollado por Celso Ibáñez &copy; 2022</p>
+  </footer>
 
-  // Consulta SELECT desde PHP
-  $query = mysqli_query($conexion, "SELECT * FROM notas");
-  // $notas = mysqli_fetch_assoc($query);
-
-  while ($nota = mysqli_fetch_assoc($query)) {
-    echo "<h2>".$nota['titulo']."</h2>";
-    echo $nota['descripcion']."<br>";
-    // var_dump($nota);
-  }
-
-  // Insertar en la bd desde PHP
-  $sql = "INSERT INTO notas VALUES(null,'Nota desde PHP','Esto es una nota de PHP','green')" ;
-  $insert = mysqli_query($conexion, $sql);
-
-  echo "<hr>";
-  if($insert){
-    echo "Datos insertados correctamente";
-  } else {
-    echo "Error: ".myslqi_error($conexion);
-  }
-?>
-
+</body>
+</html>
