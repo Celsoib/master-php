8770b6108e2c14c9ab72f279bf67a323b0e1cdac
Celsoib <90781135+Celsoib@users.noreply.github.com>
2022-02-06 23:36:33 -0300

    SECCIÓN55:CLAES ESTÁTICAS,ABSTRACTAS E INTERFACES
    constantes

--- index.php
+++ index.php
@@ -1,17 +1,39 @@
 <?php
 
-require_once "configuracion.php";
+class Usuario {
+  // LAS COSNTANTES DEBEN SER EN MAYUSCULAS
+  const URL_COMPLETA = "http://localhost";
+  public $email;
+  public $password;
 
-Configuracion::setColor("blue");
-Configuracion::setNewsletter(1);
-Configuracion::setEntorno("localhost");
+  public function getEmail(){
+    return $this->email;
+  }
 
-echo Configuracion::$color . "<br>";
-echo Configuracion::$newsletter . "<br>";
-echo Configuracion::$entorno . "<br>";
+  public function getPassword(){
+    return $this->password;
+  }
 
-$configuracion = new Configuracion();
-$configuracion::$color = "rojo";
-echo $configuracion::$color;
-var_dump($configuracion);
+  public function setEmail($email){
+    $this->email = $email;
+  }
+
+  public function setPassword($password){
+    $this->password = $password;
+  }
+
+}
+
+
+// ::self SE REFIRE A UNA PROPIEDAD A NIVEL DE CLASE
+// $this-> SE REFIERE A UNA PROPIEDAD A NIVEL DE OBJETO
+
+// TAMBIÉN FUNCIONA PQ AL SER UNA PROPIEDAD ESTÁTICA ESTÁ DEFINIDA A
+// NIVEL DE CLASE, NO A NIVEL DE OBJETO
+// echo Usuario::URL_COMPLETA;
+
+$usuario = new Usuario();
+echo $usuario::URL_COMPLETA;
+var_dump($usuario);
+
 ?>
