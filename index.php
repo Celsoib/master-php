61b5121293657031e0253bd168f7fcbed1f90293
Celsoib <90781135+Celsoib@users.noreply.github.com>
2022-02-08 01:55:41 -0300

    Sección58:Espacios de nombre en y autocargadeclass
    237. Comprobar existencia clases y métodos

--- index.php
+++ index.php
@@ -34,12 +34,51 @@ echo "<br>".$categoria->nombre;
       $this->categoria = new Categoria();
       $this->entrada = new Entrada();
     }
+
+    public function getUsuario() {
+      return $this->usuario;
+    }
+
+    public function getCategoria(){
+      return $this->categoria;
+    }
+
+    public function getEntrada(){
+      return $this->entrada;
+    }
+
+    public function setUsuario($usuario){
+      $this->usuario = $usuario;
+    }
+
+    public function setCategoria($categoria){
+      $this->categoria = $categoria;
+    }
+
+    public function setEntrada($entrada){
+      $this->entrada = $entrada;
+    }
+
   }
   // OBJETO PRINCIPAL
   $principal = new Principal();
-  var_dump($principal->usuario);
+  // var_dump($principal->usuario);
+  $metodos = get_class_methods($principal);
+
+  $busqueda = array_search('setEntrada',$metodos);
+  var_dump($busqueda); //EN PANTALLA IMPRIME EL ÍNDICE 6, SI EL MÉTODO NO EXISTE, DA false
 
   // OBJETO OTRO PAQUETE
   $usuario = new UsuarioAdmin();
   var_dump($usuario);
+
+  // COMPRUEBA SI EXISTE UNA CLASE TANTO EN EL FICHERO ACTUAL COMO DE MANERA GLOBAL EN EL PROYECTO
+  $clase = @class_exists('PanelAdministrador\Usuario2');//EL @ ESCONDE LOS WARNINGS(ERRORES) PQ SI HAY UN FALLO DE QUE NO PUEDE ABRIR UN ARCHIVO INEVITABLEMENTE ME VA A DEVOLVER UN ERROR
+  if($clase){
+    echo "<h1>La clase SÍ existe</h1>";
+  } else {
+    // PARA QUE FUNCIONE EL else TENEMOS QUE USAR include EN EL autoload.php
+    echo "<h1>La clase NO existe</h1>";
+  }
+
 ?>
