de3f0f9deeb5656f394689c2e0394261536de976
Celsoib <90781135+Celsoib@users.noreply.github.com>
2022-02-21 07:55:50 -0300

    Sección 65: Maquetación web del proyecto(html-css)
    261. CSS de la cabecera

--- index.php
+++ index.php
@@ -8,75 +8,92 @@
   <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
-  <!-- CABECERA  -->
-  <header id="header">
-    <div id="logo">
-      <img src="assets/img/camiseta.png" alt="Camiseta logo" id="logo">
-      <a href="index.php">Tienda de camisetas</a>
-    </div>
-  </header>
+  <!-- INICIO CONTAINER GENERAL  -->
+  <div id="container">
 
-  <!-- MENÚ  -->
-  <nav id="menu">
-    <ul>
-      <li>
-        <a href="#">Inicio</a>
-      </li>
-      <li>
-        <a href="#">Categoría 1</a>
-      </li>
-      <li>
-        <a href="#">Categoría 2</a>
-      </li>
-      <li>
-        <a href="#">Categoría 3</a>
-      </li>
-      <li>
-        <a href="#">Categoría 4</a>
-      </li>
-      <li>
-        <a href="#">Categoría 5</a>
-      </li>
-    </ul>
-  </nav>
-
-  <div id="content">
-    <!-- BARRA LATERAL  -->
-    <div class="block_aside" id="login">
-      <form action="#" method="POST">
-        <p>
-          <label for="email">Email</label>
-          <input type="email" name="email">
-        </p>
-        <p>
-          <label for="password">Contraseña</label>
-          <input type="password" name="password">
-        </p>
-        <input type="submit" value="Enviar">
-      </form>
-
-      <a href="#">Mis pedidos</a>
-      <a href="#">Gestionar pedidos</a>
-      <a href="#">Gestionar categorías</a>
-
-    </div>
-
-    <!-- CONTENIDO CENTRAL  -->
-    <div id="central">
-      <div class="product">
-        <img src="assets/img/camiseta.png" alt="Foto camiseta">
-        <h2>Camiseta Azul Ancha</h2>
-        <p>30.000 Gs</p>
-        <a href="#">Comprar</a>
+    <!-- CABECERA  -->
+    <header id="header">
+      <div id="logo">
+        <img src="assets/img/camiseta.png" alt="Camiseta logo" id="logo">
+        <a href="index.php">Tienda de camisetas</a>
       </div>
+    </header>
+
+    <!-- MENÚ  -->
+    <nav id="menu">
+      <ul>
+        <li>
+          <a href="#">Inicio</a>
+        </li>
+        <li>
+          <a href="#">Categoría 1</a>
+        </li>
+        <li>
+          <a href="#">Categoría 2</a>
+        </li>
+        <li>
+          <a href="#">Categoría 3</a>
+        </li>
+        <li>
+          <a href="#">Categoría 4</a>
+        </li>
+        <li>
+          <a href="#">Categoría 5</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div id="content">
+      <!-- BARRA LATERAL  -->
+      <div class="block_aside" id="login">
+        <form action="#" method="POST">
+          <p>
+            <label for="email">Email</label>
+            <input type="email" name="email">
+          </p>
+          <p>
+            <label for="password">Contraseña</label>
+            <input type="password" name="password">
+          </p>
+          <input type="submit" value="Enviar">
+        </form>
+
+        <a href="#">Mis pedidos</a>
+        <a href="#">Gestionar pedidos</a>
+        <a href="#">Gestionar categorías</a>
+
+      </div>
+
+      <!-- CONTENIDO CENTRAL  -->
+      <div id="central">
+        <div class="product">
+          <img src="assets/img/camiseta.png" alt="Foto camiseta">
+          <h2>Camiseta Azul Ancha</h2>
+          <p>30.000 Gs</p>
+          <a href="#">Comprar</a>
+        </div>
+        <div class="product">
+          <img src="assets/img/camiseta.png" alt="Foto camiseta">
+          <h2>Camiseta Azul Ancha</h2>
+          <p>30.000 Gs</p>
+          <a href="#">Comprar</a>
+        </div>
+        <div class="product">
+          <img src="assets/img/camiseta.png" alt="Foto camiseta">
+          <h2>Camiseta Azul Ancha</h2>
+          <p>30.000 Gs</p>
+          <a href="#">Comprar</a>
+        </div>
+      </div>
+
     </div>
 
+
+    <!-- PIE DE PÁGINA  -->
+    <footer id="footer">
+      <p>Desarrollado por Celso Ibáñez WEB &copy; <?=date('Y')?></p>
+    </footer>
+  <!-- FIN CONTAINER GENERAL    -->
   </div>
-
-
-  <!-- PIE DE PÁGINA  -->
-  <footer id="footer">
-    <p>Desarrollado por Celso Ibáñez WEB &copy; <?=date('Y')?></p>
-  </footer>
 </body>
 </html>
