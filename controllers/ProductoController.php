b6caff4040d4b90b183c88604ccbc9911f36b5e6
Celsoib <90781135+Celsoib@users.noreply.github.com>
2022-03-01 10:02:30 -0300

    Sección 70: Productos de la tienda
    280. Formulario para crear productos
    10 min

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -17,7 +17,24 @@ class productoController {
 
     require_once "views/producto/gestion.php";
   }
-}
+
+  public function crear(){
+    Utils::isAdmin();
+
+    require_once "views/producto/crear.php";
+  }
+
+  public function save(){
+    Utils::isAdmin();
+
+    if(isset($_POST)){
+      var_dump($_POST);
+    }
+  }
+
+
+
+} //FIN CLASE
 
 
 ?>
---
+++ views/producto/crear.php
@@ -0,0 +1,42 @@
+<h1>Crear nuevos productos</h1>
+
+<div class="form_container">
+
+  <form action="<?=base_url?>producto/save" method="POST">
+
+    <p>
+      <label for="nombre">Nombre</label>
+      <input type="text" name="nombre">
+    </p>
+    <p>
+      <label for="descripcion">Descripción</label>
+      <textarea name="descripcion"></textarea>
+    </p>
+    <p>
+      <label for="precio">Precio</label>
+      <input type="text" name="precio">
+    </p>
+    <p>
+      <label for="stock">Stock</label>
+      <input type="number" name="stock">
+    </p>
+    <p>
+      <label for="categoria">Categoria</label>
+      <?php $categorias = Utils::showCategorias(); ?>
+      <select name="categoria">
+        <?php while($cat = $categorias->fetch_object()):?>
+          <option value="<?=$cat->id?>">
+            <?=$cat->nombre?>
+          </option>
+        <?php endwhile; ?>
+      </select>
+    </p>
+    <p>
+      <label for="imagen">Imagen</label>
+      <input type="file" name="imagen">
+    </p>
+
+    <input type="submit" value="Guardar">
+
+  </form>
+</div>
