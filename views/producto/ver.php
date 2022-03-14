69b971ef860b422fd9dd8560e33aa57cce50d4b1
Celsoib <90781135+Celsoib@users.noreply.github.com>
2022-03-14 16:54:50 -0300

    SECCIÓN 71: MOSTRAR PRODUCTOS
    291. Maquetación CSS de la pagina del producto
    8 min

--- views/producto/ver.php
+++ views/producto/ver.php
@@ -1,15 +1,20 @@
 <?php if(isset($product)): ?>
   <h1><?=$product->nombre?></h1>
+  <div class="detail-product">
+    <div class="image">
+      <?php if($product->imagen != null): ?>
+        <img src="<?=base_url?>uploads/images/<?=$product->imagen?>" alt="Foto camiseta">
+      <?php else: ?>
+        <img src="<?=base_url?>assets/img/camiseta.png" alt="Foto camiseta">
+      <?php endif; ?>
+    </div>
 
-  <?php if($product->imagen != null): ?>
-    <img src="<?=base_url?>uploads/images/<?=$product->imagen?>" alt="Foto camiseta">
-  <?php else: ?>
-    <img src="<?=base_url?>assets/img/camiseta.png" alt="Foto camiseta">
-  <?php endif; ?>
-
-  <p><?=$product->descripcion?></p>
-  <p><?=$product->precio?></p>
-  <a href="#" class="button">Comprar</a>
+    <div class="data">
+      <p class="description"><?=$product->descripcion ?></p>
+      <p class="price"><?=$product->precio ?> Gs</p>
+      <a href="#" class="button">Comprar</a>
+    </div>
+  </div>
 
 <?php else: ?>
   <h1>El producto no existe</h1>
